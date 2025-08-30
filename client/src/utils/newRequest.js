38fe8baef82b7f250f32bd0919b72671496b2ac1
testinglife001 <testing.life001@gmail.com>
2025-08-31 05:11:29 +0600

    second commit to github and attempt two, client .env updated

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`, // ✅ dynamic
   withCredentials: true, // optional if using cookies
 });
 
@@ -16,4 +16,25 @@ newRequest.interceptors.request.use((config) => {
 
 export default newRequest;
 
+
+
+
+/*
+import axios from "axios";
+
+const newRequest = axios.create({
+  baseURL: "http://localhost:8800/api",
+  withCredentials: true, // optional if using cookies
+});
+
+newRequest.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default newRequest;
+*/
 
