029e86986564ef098aeb273335e1319b0dc8b48a
testinglife001 <testing.life001@gmail.com>
2025-08-31 05:27:06 +0600

    updateapi.js in client

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8800/api',
+  // baseURL: 'http://localhost:8800/api',
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`, // ✅ dynamic
   withCredentials: true,
 });
 
